0227372414c30f77e0c12f450013249a216779d4
GreenGargoyle <gkenhur@gmail.com>
2020-06-29 19:30:47 +0100

    US2 - Simple movement

---
+++ Giga/Assets/Scripts/Player/PlayerCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraScript : MonoBehaviour
+{
+
+    //Target for which the camera will follow.
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+        {
+            throw new System.Exception("PlayerCameraScript - target variable not provided.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
