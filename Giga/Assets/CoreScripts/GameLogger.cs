853c47f18bc1fad39928745ece3744a58f65d65c
GreenGargoyle <gkenhur@gmail.com>
2020-06-30 18:08:43 +0100

    US3 - Player Camera Added

---
+++ Giga/Assets/CoreScripts/GameLogger.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreScripts
+{
+	public static class GameLogger
+	{
+        private static bool _debugEnabled = true;
+        private static bool _warnEnabled = true;
+        private static bool _errorEnabled = true;
+
+        #region Debug
+        public static void debug(string message)
+        {
+            if (!_debugEnabled) { return; }
+            Debug.Log(message);
+        }
+
+        public static void debug(string message, Object context)
+        {
+            if (!_debugEnabled) { return; }
+            Debug.Log(message, context);
+        }
+
+        public static void setDebugEnabled(bool param)
+        {
+            _debugEnabled = param;
+        }
+
+        public static bool getDebugEnabled()
+        {
+            return _debugEnabled;
+        }
+        #endregion
+
+        #region Warn
+        public static void warn(string message)
+        {
+            if (!_warnEnabled) { return; }
+            Debug.LogWarning(message);
+        }
+
+        public static void warn(string message, Object context)
+        {
+            if (!_warnEnabled) { return; }
+            Debug.LogWarning(message, context);
+        }
+
+        public static void setWarnEnabled(bool param)
+        {
+            _warnEnabled = param;
+        }
+
+        public static bool getWarnEnabled()
+        {
+            return _warnEnabled;
+        }
+        #endregion
+
+        #region Error
+        public static void error(string message)
+        {
+            if (!_errorEnabled) { return; }
+            Debug.LogError(message);
+        }
+
+        public static void error(string message, Object context)
+        {
+            if (!_errorEnabled) { return; }
+            Debug.LogError(message, context);
+        }
+        
+        public static void setErrorEnabled(bool param)
+        {
+            _errorEnabled = param;
+        }
+
+        public static bool getErrorEnabled()
+        {
+            return _errorEnabled;
+        }
+        #endregion
+    }
+}
+
---
+++ Giga/Assets/Player/PlayerCameraScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CoreScripts;
+
+namespace Player
+{
+    public class PlayerCameraScript : MonoBehaviour
+    {
+        //Target for which the camera will follow.
+        [SerializeField] private Transform target;
+
+        //Conrols how far away the camera is from the target.
+        [SerializeField] private float cameraDistanceFromTarget = 5.0f;
+
+        //Controls how close the camera can ever be to the target.
+        [SerializeField] private float minDistanceFromTarget = 0.5f;
+
+        //The height the camera is above the target.
+        [SerializeField] private float cameraHeight = 1.0f;
+
+        [SerializeField] private float distanceDampening = 10.0f;
+
+        [SerializeField] private float rotationalDampening = 10.0f;
+
+        //Transform of the camera.
+        private Transform _myTransform;
+
+
+        void Awake()
+        {
+            _myTransform = transform;
+        }
+
+        void Start()
+        {
+            if (target == null)
+            {
+                warn("No target provided to the PlayerCameraScript.");
+                target = new GameObject().transform;
+            }
+        }
+
+        void LateUpdate()
+        {
+            //Translate via lerp to newPosition.
+            Vector3 newPosition = target.position + (target.rotation * new Vector3(0.0f, 0.0f, -cameraDistanceFromTarget));
+            Vector3 currentPosition = Vector3.Lerp(_myTransform.position, newPosition, distanceDampening * Time.deltaTime);
+
+            //Rotate via slerp to newRotation.
+            Quaternion newRotation = Quaternion.LookRotation(target.position - _myTransform.position, target.up);
+            Quaternion currentRotation = Quaternion.Slerp(_myTransform.rotation, newRotation, rotationalDampening * Time.deltaTime);
+
+            _myTransform.SetPositionAndRotation(currentPosition, currentRotation);
+        }
+
+        private void warn(string message)
+        {
+            GameLogger.warn(message);
+        }
+    }
+}
+
